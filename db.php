32cca449c8b1305bba8e26a25e51c18023939614
Emery Frondorf <masonfrondorf@gmail.com>
2024-12-04 15:15:01 -0500

    adding database file

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$host='localhost';
+$dbname='karen_social';
+$user='root';
+$pass='';
+
+$options=[
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+$db = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8mb4',$user,$pass);
+?>
